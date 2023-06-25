8ec54b8772def54a4406d9b492479522d92a786c
nanaSpace <diana.rodriguez.jackson@gmail.com>
2023-06-24 23:13:38 -0600

    inicio del proyecto

---
+++ Proyecto grupo 2.py
@@ -0,0 +1,13 @@
+#Trabajo grupo 2 mensajeria Fidelitas
+
+
+
+#Parte Diana y Franco (Registrar una cuenta de usuario)
+
+
+
+#Parte Daril y Alex(Registro de factura electrónica)
+
+
+
+#Parte Martines (Creación de paquetes)
